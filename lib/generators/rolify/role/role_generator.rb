03f2a8e7e68b187702c86e00a361944c06fed5a6
Florent Monbillard <f.monbillard@gmail.com>
2011-08-25 22:01:48 +0200

    added dynamic_shortcuts parameter in the generators to disable dynamic methods creation (due to omniauth incompatibility)

--- lib/generators/rolify/role/role_generator.rb
+++ lib/generators/rolify/role/role_generator.rb
@@ -8,6 +8,7 @@ module Rolify
       source_root File.expand_path('../templates', __FILE__)
       argument :role_cname, :type => :string, :default => "Role"
       argument :user_cname, :type => :string, :default => "User"
+      argument :dynamic_shortcuts, :type => :boolean, :default => true
 
       desc "Generates a model with the given NAME and a migration file."
 
@@ -15,7 +16,7 @@ module Rolify
         template "role.rb", "app/models/#{role_cname.downcase}.rb"
         inject_into_class(model_path, user_cname.camelize) do
           "  include Rolify::Roles\n" + 
-          "  extend Rolify::Reloaded\n" + 
+          "#{"  extend Rolify::Reloaded\n" if dynamic_shortcuts}" + 
           "  has_and_belongs_to_many :roles#{", :class_name => \"" + role_cname.camelize + "\"" if role_cname != "Role"}, :join_table => :#{user_cname.tableize + "_" + role_cname.tableize}\n"
         end
       end
--- lib/generators/rolify/role/templates/initializer.rb
+++ lib/generators/rolify/role/templates/initializer.rb
@@ -1,2 +1,3 @@
 Rolify.user_cname = <%= user_cname.camelize %>
 Rolify.role_cname = <%= role_cname.camelize %>
+Rolify.Rolify.dynamic_shortcuts = <%= dynamic_shortcuts.to_s %>
