43b0bddc2a0ae12fcd1985134bf7a2cdba4b262d
Florent Monbillard <f.monbillard@gmail.com>
2011-10-18 01:05:27 -0400

    clean up the initializer code, detecting if it's loaded by Rails::Server or Rails::Console (#16)

--- lib/generators/rolify/role/templates/initializer.rb
+++ lib/generators/rolify/role/templates/initializer.rb
@@ -1,3 +1,3 @@
 Rolify.user_cname = <%= user_cname.camelize %>
 Rolify.role_cname = <%= role_cname.camelize %>
-Rolify.dynamic_shortcuts = <%= options[:dynamic_shortcuts].to_s %> if ARGV[0] != "db:migrate"
+Rolify.dynamic_shortcuts = <%= options[:dynamic_shortcuts].to_s %> if defined? Rails::Server || defined? Rails::Console
