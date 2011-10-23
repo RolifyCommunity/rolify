9ac33feec91f75a449384ba797767f875a4aa94e
Florent Monbillard <f.monbillard@gmail.com>
2011-10-23 18:31:03 -0400

    Revert the subselect query (#15) as it breaks the new edge cases specs (#19)
    This reverts commit 8b3e251b91614605f41c9b4f954895cde0776866.

--- lib/rolify/role.rb
+++ lib/rolify/role.rb
@@ -43,9 +43,16 @@ module Rolify
     end
 
     def has_all_roles?(*args)
-      conditions, values = sql_conditions(args)
-      requested_roles = Rolify.role_cname.where([ conditions.join(' OR '), *values ])
-      self.roles.where(:id => requested_roles.select(:id)).size == args.size
+      args.each do |arg|
+        if arg.is_a? Hash
+          return false if !self.has_role?(arg[:name], arg[:resource])
+        elsif arg.is_a? String
+          return false if !self.has_role?(arg)
+        else
+          raise ArgumentError, "Invalid argument type: only hash or string allowed"
+        end
+      end
+      true
     end
 
     def has_any_role?(*args)
