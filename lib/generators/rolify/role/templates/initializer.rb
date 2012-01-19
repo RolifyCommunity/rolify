8d5a27e709a80a293aec6dbc89e7dd77d4eb188f
Florent Monbillard <f.monbillard@gmail.com>
2012-01-18 23:09:10 -0500

    fixed the initializer file to properly load dynamic shortcuts closes #29

--- lib/generators/rolify/role/templates/initializer.rb
+++ lib/generators/rolify/role/templates/initializer.rb
@@ -6,5 +6,5 @@ Rolify.configure do |c|
   # By default ORM adapter is ActiveRecord. uncomment to use mongoid
   <%= "# " if orm_adapter == "active_record" %>c.use_mongoid
   # Dynamic shortcuts for Role class (user.is_admin? like methods). Default is: false
-  <%= "# " if !options[:dynamic_shortcuts] %>c.dynamic_shortcuts = <%= options[:dynamic_shortcuts] == true ? true : false %> if defined?(Rails::Server) == true || defined?(Rails::Console) == true
+  <%= "# " if !options[:dynamic_shortcuts] %>c.dynamic_shortcuts = <%= options[:dynamic_shortcuts] == true ? true : false %> if !defined?(Rails::Server) || !defined?(Rails::Console)
 end
