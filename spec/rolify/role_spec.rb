549d74a21146d362687a93b2a0b81dd99aa330af
Florent Monbillard <f.monbillard@gmail.com>
2012-02-06 15:39:15 -0500

    forgot to uncomment specs...

--- spec/rolify/role_spec.rb
+++ spec/rolify/role_spec.rb
@@ -150,14 +150,14 @@ shared_examples_for "Rolify module" do |dynamic|
     end
     
     it "should get all resources binded to a specific role and a specific user having one this role" do
-      #Forum.with_role("forum", @admin).should include(Forum.first)
-      #Forum.with_role("forum", @tourist).should include(Forum.last)
-      #Forum.with_role("godfather", @admin).should == Forum.all
+      Forum.with_role("forum", @admin).should include(Forum.first)
+      Forum.with_role("forum", @tourist).should include(Forum.last)
+      Forum.with_role("godfather", @admin).should == Forum.all
       Forum.with_role("godfather", @tourist).should be_empty
-      #Group.with_role("group", @admin).should include(Group.last)
-      #Group.with_role("group", @admin).should_not include(Group.first)
-      #Forum.with_role("group", @tourist).should include(Forum.first)
-      #Forum.with_role("group", @tourist).should_not include(Forum.last)
+      Group.with_role("group", @admin).should include(Group.last)
+      Group.with_role("group", @admin).should_not include(Group.first)
+      Forum.with_role("group", @tourist).should include(Forum.first)
+      Forum.with_role("group", @tourist).should_not include(Forum.last)
     end
   end
 
