b32249cffd8ecfd1cb9e9c58d39ca606ea41fadf
Florent Monbillard <f.monbillard@gmail.com>
2012-03-06 17:59:24 -0500

    keep refactoring specs: dynamic methods refs #38

--- spec/rolify/dynamic.rb
+++ spec/rolify/dynamic.rb
@@ -8,60 +8,97 @@ shared_examples_for Rolify::Dynamic do
     Group.resourcify :role_cname => role_cname
   end
   
-  before(:all) do
-    @admin = Rolify.user_cname.first
-    @admin.has_role "admin"
-    @admin.has_role "moderator", Forum.first
-  end
-  
-  before do
-    @moderator = Rolify.user_cname.where(:login => "moderator").first
-    @moderator.has_role "moderator", Forum.first
-    @moderator.has_role "soldier"
-  end
-  
-  before do
-    @manager = Rolify.user_cname.where(:login => "god").first
-    @manager.has_role "manager", Forum
-    @manager.has_role "moderator", Forum.first
-    @manager.has_role "moderator", Forum.last
-    @manager.has_role "warrior"
-  end
-  
-  it "should respond to dynamic methods" do
-    @admin.should respond_to(:is_admin?).with(0).arguments
-    @admin.should respond_to(:is_moderator_of?).with(1).arguments
-  end
-  
-  it "should not respond to any unknown methods" do
-    @admin.should_not respond_to(:is_god?)
-  end
-  
-  it "should create a new dynamic method if role exists in database" do 
-    other_guy = Rolify.user_cname.last
-    other_guy.has_role "superman"
-    @admin.should respond_to(:is_superman?).with(0).arguments
-    other_guy.has_role("batman", Forum.first)
-    @admin.should respond_to(:is_batman_of?).with(1).arguments
-    @admin.should respond_to(:is_batman?).with(0).arguments
-  end
-  
-  it "should be able to use dynamic shortcut" do 
-    @admin.is_admin?.should be(true)
-  end
+  context "using a global role" do
+    subject do 
+      admin = Rolify.user_cname.first
+      admin.has_role "admin"
+      admin.has_role "moderator", Forum.first
+      admin
+    end
 
-  it "should be able to use dynamic shortcut" do 
-    @moderator.is_moderator?.should be(false)
-    @moderator.is_moderator_of?(Forum.first).should be(true)
-    @moderator.is_moderator_of?(Forum.last).should be(false)
-    @moderator.is_moderator_of?(Forum).should be(false)
-  end
+    it { should respond_to(:is_admin?).with(0).arguments }
+    it { should respond_to(:is_moderator_of?).with(1).arguments }
+    it { should_not respond_to(:is_god?) }
 
-  it "should be able to use dynamic shortcut" do
-    @manager.is_manager?.should be(false)
-    @manager.is_manager_of?(Forum.first).should be(true)
-    @manager.is_manager_of?(Forum.last).should be(true)
-    @manager.is_manager_of?(Forum).should be(true)
-    @manager.is_manager_of?(Group).should be(false)
+    it { subject.is_admin?.should be(true) }
+    it { subject.is_admin?.should be(true) }
+    it { subject.is_admin?.should be(true) }
+  end
+  
+  context "using a resource scoped role" do
+    subject do 
+      moderator = Rolify.user_cname.where(:login => "moderator").first
+      moderator.has_role "moderator", Forum.first
+      moderator
+    end
+    
+    it { should respond_to(:is_admin?).with(0).arguments }
+    it { should respond_to(:is_moderator?).with(0).arguments }
+    it { should respond_to(:is_moderator_of?).with(1).arguments }
+    
+    it { subject.is_moderator?.should be(false) }
+    it { subject.is_moderator_of?(Forum).should be(false) }
+    it { subject.is_moderator_of?(Forum.first).should be(true) }
+    it { subject.is_moderator_of?(Forum.last).should be(false) }
+    it { subject.is_moderator_of?(Group).should be(false) }
+    it { subject.is_moderator_of?(Group.first).should be(false) }
+    it { subject.is_moderator_of?(Group.last).should be(false) }
+  end
+  
+  context "using a class scoped role" do
+    subject do 
+      manager = Rolify.user_cname.where(:login => "god").first
+      manager.has_role "manager", Forum
+      manager
+    end
+    
+    it { should respond_to(:is_admin?).with(0).arguments }
+    it { should respond_to(:is_moderator?).with(0).arguments }
+    it { should respond_to(:is_moderator_of?).with(1).arguments }
+    it { should respond_to(:is_manager?).with(0).arguments }
+    it { should respond_to(:is_manager_of?).with(1).arguments }
+    
+    it { subject.is_manager?.should be(false) }
+    it { subject.is_manager_of?(Forum).should be(true) }
+    it { subject.is_manager_of?(Forum.first).should be(true) }
+    it { subject.is_manager_of?(Forum.last).should be(true) }
+    it { subject.is_manager_of?(Group).should be(false) }
+    it { subject.is_manager_of?(Group.first).should be(false) }
+    it { subject.is_manager_of?(Group.last).should be(false) }
+  end
+  
+  context "if the role doesn't exist in the database" do
+    
+    subject { Rolify.user_cname.first }
+    
+    context "using a global role" do
+      before(:all) do
+        other_guy = Rolify.user_cname.last
+        other_guy.has_role "superman"
+      end
+  
+      it { should respond_to(:is_superman?).with(0).arguments }
+      it { should_not respond_to(:is_superman?).with(1).arguments }
+      
+      it { subject.is_superman?.should be(false) }
+    end
+    
+    context "using a resource scope role" do
+      before(:all) do
+        other_guy = Rolify.user_cname.last
+        other_guy.has_role("batman", Forum.first)
+      end
+      
+      it { should respond_to(:is_batman?).with(0).arguments}
+      it { should respond_to(:is_batman_of?).with(1).arguments }
+      
+      it { subject.is_batman?.should be(false) }
+      it { subject.is_batman_of?(Forum).should be(false) }
+      it { subject.is_batman_of?(Forum.first).should be(false) }
+      it { subject.is_batman_of?(Forum.last).should be(false) }
+      it { subject.is_batman_of?(Group).should be(false) }
+      it { subject.is_batman_of?(Group.first).should be(false) }
+      it { subject.is_batman_of?(Group.last).should be(false) }
+    end
   end
 end
