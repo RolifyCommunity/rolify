5066b2ee421a211d054e3fc67cd3584fc29cc166
Florent Monbillard <f.monbillard@gmail.com>
2012-03-23 22:22:43 -0400

    enhanced code indentation [ci skip]

--- lib/rolify/role.rb
+++ lib/rolify/role.rb
@@ -4,19 +4,19 @@ require 'rolify/resource'
 
 module Rolify
   extend Configure
-  
+
   module Role  
     def rolify(options = { :role_cname => 'Role' })
-       include InstanceMethods
-       extend Dynamic if Rolify.dynamic_shortcuts
-       rolify_options = { :class_name => options[:role_cname].camelize }
-       rolify_options.merge!({ :join_table => "#{self.to_s.tableize}_#{options[:role_cname].tableize}" }) if Rolify.orm == "active_record"
-       has_and_belongs_to_many :roles, rolify_options
+      include InstanceMethods
+      extend Dynamic if Rolify.dynamic_shortcuts
+      rolify_options = { :class_name => options[:role_cname].camelize }
+      rolify_options.merge!({ :join_table => "#{self.to_s.tableize}_#{options[:role_cname].tableize}" }) if Rolify.orm == "active_record"
+      has_and_belongs_to_many :roles, rolify_options
 
-       load_dynamic_methods if Rolify.dynamic_shortcuts
-       Rolify.role_cname = options[:role_cname]
+      load_dynamic_methods if Rolify.dynamic_shortcuts
+      Rolify.role_cname = options[:role_cname]
     end
-    
+
     def resourcify(options = { :role_cname => 'Role' })
       resourcify_options = { :class_name => options[:role_cname].camelize }
       resourcify_options.merge!({ :as => :resource })
@@ -24,13 +24,13 @@ module Rolify
       include Resource
     end
   end
-  
+
   module InstanceMethods
     def has_role(role_name, resource = nil)
       role = Rolify.adapter.find_or_create_by(role_name, 
                                               (resource.is_a?(Class) ? resource.to_s : resource.class.name if resource), 
                                               (resource.id if resource && !resource.is_a?(Class)))
-                                              
+
       if !roles.include?(role)
         self.class.define_dynamic_method(role_name, resource) if Rolify.dynamic_shortcuts
         Rolify.adapter.add(self, role)
@@ -38,7 +38,7 @@ module Rolify
       role
     end
     alias_method :grant, :has_role
-  
+
     def has_role?(role_name, resource = nil)
       Rolify.adapter.find(self.roles, role_name, resource).size > 0
     end
@@ -59,12 +59,12 @@ module Rolify
     def has_any_role?(*args)
       Rolify.adapter.where(self.roles, args).size > 0
     end
-  
+
     def has_no_role(role_name, resource = nil)
       Rolify.adapter.remove(self.roles, role_name, resource)
     end
     alias_method :revoke, :has_no_role
-  
+
     def roles_name
       self.roles.select(:name).map { |r| r.name }
     end
@@ -79,7 +79,7 @@ module Rolify
       end unless !Rolify.dynamic_shortcuts
       super
     end
-    
+
     def respond_to?(method, include_private = false)
       if Rolify.dynamic_shortcuts && (method.to_s.match(/^is_(\w+)_of[?]$/) || method.to_s.match(/^is_(\w+)[?]$/))
         query = Rolify.role_cname.where(:name => $1)
@@ -91,5 +91,5 @@ module Rolify
       end
     end
   end
-  
+
 end
