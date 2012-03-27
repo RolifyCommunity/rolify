7900c23d2979b7e08f619fca5c7ff26a8a71827d
Florent Monbillard <f.monbillard@gmail.com>
2012-03-27 15:59:05 -0400

    removed all Rolify.user_cname and Rolify.role_cname usage in specs added some specs to enhance test coverage of Mongoid support closes #42

--- spec/rolify/config_spec.rb
+++ spec/rolify/config_spec.rb
@@ -1,51 +1,24 @@
 require "spec_helper"
 
+class ARUser < ActiveRecord::Base
+  extend Rolify
+end
+
+class MUser
+  include Mongoid::Document
+  extend Rolify
+end
+
 describe Rolify do
   before do
     Rolify.use_defaults
   end
 
-  #describe :user_cname do 
-  #  context "using defaults values" do
-  #    subject { Rolify.user_cname }
-  #
-  #    it { should be(User) }
-  #  end
-  #
-  #  context "using custom values" do
-  #    before do
-  #      Rolify.user_cname = "Customer"
-  #    end
-  #
-  #    subject { Rolify.user_cname }
-  #
-  #    it { should be(Customer) }
-  #  end
-  #end
-  #
-  #describe :role_cname do 
-  #  context "using defaults values" do
-  #    subject { Rolify.role_cname }
-  #
-  #    it { should be(Role) }
-  #  end
-  #
-  #  context "using custom values" do
-  #    before do
-  #      Rolify.role_cname = "Privilege"
-  #    end
-  #
-  #    subject { Rolify.role_cname }
-  #
-  #    it { should be(Privilege) }
-  #  end
-  #end
-
   describe :dynamic_shortcuts do 
     context "using defaults values" do
       subject { Rolify.dynamic_shortcuts }
 
-      it { should be(false) }
+      it { should be_false }
     end
 
     context "using custom values" do
@@ -55,7 +28,7 @@ describe Rolify do
 
       subject { Rolify.dynamic_shortcuts }
 
-      it { should be(true) }
+      it { should be_true }
     end
   end
 
@@ -64,6 +37,26 @@ describe Rolify do
       subject { Rolify.orm }
 
       it { should eq("active_record") }
+      
+      context "on the User class" do
+        before do
+          ARUser.rolify
+        end
+        
+        subject { ARUser }
+        
+        its("adapter.class") { should be(Rolify::Adapter::ActiveRecord) }
+      end
+      
+      context "on the Forum class" do
+        before do
+          Forum.resourcify
+        end
+        
+        subject { Forum }
+        
+        its("adapter.class") { should be(Rolify::Adapter::ActiveRecord) }
+      end
     end
 
     context "using custom values" do
@@ -75,6 +68,26 @@ describe Rolify do
         subject { Rolify.orm }
 
         it { should eq("mongoid") }
+        
+        context "on the User class" do
+          before do
+            MUser.rolify
+          end
+
+          subject { MUser }
+          
+          its("adapter.class") { should be(Rolify::Adapter::Mongoid) }
+        end
+
+        context "on the Forum class" do
+          before do
+            Forum.resourcify
+          end
+
+          subject { Forum }
+
+          its("adapter.class") { should be(Rolify::Adapter::Mongoid) }
+        end
       end
       
       context "using :orm setter method" do
@@ -85,41 +98,62 @@ describe Rolify do
         subject { Rolify.orm }
 
         it { should eq("mongoid") }
+        
+        context "on the User class" do
+          before do
+            MUser.rolify
+          end
+
+          subject { MUser }
+          
+          its("adapter.class") { should be(Rolify::Adapter::Mongoid) }
+        end
+
+        context "on the Forum class" do
+          before do
+            Forum.resourcify
+          end
+
+          subject { Forum }
+
+          its("adapter.class") { should be(Rolify::Adapter::Mongoid) }
+        end
       end
     end
   end
   
-  #describe :adapter do
-  #  context "using defaults values" do
-  #    subject { Rolify.adapter }
-  #
-  #    it { should be(Rolify::Adapter::ActiveRecord) }
-  #  end
-  #  
-  #  context "using custom values" do
-  #    before do
-  #      Rolify.use_mongoid
-  #    end
-  #    subject { Rolify.adapter }
-  #
-  #    it { should be(Rolify::Adapter::Mongoid) }
-  #  end
-  #end
-  
   describe :configure do
     before do
       Rolify.configure do |r|
-        #r.role_cname = "Privilege"
-        #r.user_cname = "Customer"
         r.dynamic_shortcuts = true
         r.orm = "mongoid"
       end
     end
     
-    #its(:role_cname) { should be(Privilege) }
-    #its(:user_cname) { should be(Customer) }
-    its(:dynamic_shortcuts) { should be(true) }
+    its(:dynamic_shortcuts) { should be_true }
     its(:orm) { should eq("mongoid") }
-    #its(:adapter) { should be(Rolify::Adapter::Mongoid) }
+    
+    context "on the User class" do
+      before do
+        MUser.rolify
+      end
+
+      subject { MUser }
+      
+      it { should satisfy { |u| u.include? Rolify::Role }}
+      it { should satisfy { |u| u.singleton_class.include? Rolify::Dynamic } }
+      its("adapter.class") { should be(Rolify::Adapter::Mongoid) }
+    end
+
+    context "on the Forum class" do
+      before do
+        Forum.resourcify
+      end
+
+      subject { Forum }
+      
+      it { should satisfy { |u| u.include? Rolify::Resource }}
+      its("adapter.class") { should be(Rolify::Adapter::Mongoid) }
+    end
   end
 end
