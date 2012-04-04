d73b64e99fe41479bf11cdc2996db2407a261c9c
Florent Monbillard <f.monbillard@gmail.com>
2012-04-03 21:33:59 -0400

    removed block argument for deprecate method as it's not needed

--- lib/rolify/utils.rb
+++ lib/rolify/utils.rb
@@ -1,9 +1,9 @@
 module Rolify
   module Utils
     def deprecate(old_method, new_method)
-      define_method(old_method) do |*args, &block|
+      define_method(old_method) do |*args|
         warn "[DEPRECATION] #{caller.first}: `#{old_method}` is deprecated.  Please use `#{new_method}` instead."
-        send(new_method, *args, &block)
+        send(new_method, *args)
       end
     end
   end
