8ceb8f381145f579bb34b112e28149cb6ece561a
Ullrich Schäfer <ullrich@seidbereit.de>
2012-04-06 21:19:57 +0200

    Adding indexes to generator for mongoid

--- lib/generators/rolify/role/templates/role-mongoid.rb
+++ lib/generators/rolify/role/templates/role-mongoid.rb
@@ -5,4 +5,13 @@ class <%= role_cname.camelize %>
   belongs_to :resource, :polymorphic => true
   
   field :name, :type => String
+  index :name, unique: true
+  index(
+    [
+      [:name, Mongo::ASCENDING],
+      [:resource_type, Mongo::ASCENDING],
+      [:resource_id, Mongo::ASCENDING]
+    ],
+    unique: true
+  )
 end
