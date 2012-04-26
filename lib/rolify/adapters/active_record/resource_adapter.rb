da1ec59acda83b9114aeb410c99b1ce6e0866648
Florent Monbillard <f.monbillard@gmail.com>
2012-04-26 13:12:18 -0400

    fixed a syntax error using MySQL (double quotes around table names) fixes #65

--- lib/rolify/adapters/active_record/resource_adapter.rb
+++ lib/rolify/adapters/active_record/resource_adapter.rb
@@ -4,13 +4,19 @@ module Rolify
   module Adapter   
     class ResourceAdapter < ResourceAdapterBase
       def resources_find(roles_table, relation, role_name)
-        resources = relation.joins("INNER JOIN \"#{roles_table}\" ON \"#{roles_table}\".\"resource_type\" = '#{relation.to_s}'")
-        resources = resources.where("#{roles_table}.name = ? AND #{roles_table}.resource_type = ?", role_name, relation.to_s)
+        resources = relation.joins("INNER JOIN #{quote(roles_table)} ON #{quote(roles_table)}.resource_type = '#{relation.to_s}'")
+        resources = resources.where("#{quote(roles_table)}.name = ? AND #{quote(roles_table)}.resource_type = ?", role_name, relation.to_s)
         resources
       end
 
       def in(relation, roles)
-        relation.where("#{role_class.to_s.tableize}.id IN (?) AND ((resource_id = #{relation.table_name}.id) OR (resource_id IS NULL))", roles)
+        relation.where("#{quote(role_class.to_s.tableize)}.id IN (?) AND ((resource_id = #{quote(relation.table_name)}.id) OR (resource_id IS NULL))", roles)
+      end
+      
+      private
+      
+      def quote(column)
+         ActiveRecord::Base.connection.quote_column_name column
       end
     end
   end
