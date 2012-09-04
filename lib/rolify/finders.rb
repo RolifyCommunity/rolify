93baa35312d053e4b137deb747291d8e0f6f62da
Florent Monbillard <f.monbillard@gmail.com>
2012-09-04 15:01:25 -0400

    clean up some code duplication in finders

--- lib/rolify/finders.rb
+++ lib/rolify/finders.rb
@@ -6,14 +6,7 @@ module Rolify
 
     def with_all_roles(*args)
       users = []
-      args.each do |arg|
-        if arg.is_a? Hash
-          users_to_add = self.with_role(arg[:name], arg[:resource])
-        elsif arg.is_a?(String) || arg.is_a?(Symbol)
-          users_to_add = self.with_role(arg)
-        else
-          raise ArgumentError, "Invalid argument type: only hash or string or symbol allowed"
-        end
+      parse_args(args, users) do |users_to_add|
         users = users_to_add if users.empty?
         users &= users_to_add
         return [] if users.empty?
@@ -23,17 +16,25 @@ module Rolify
 
     def with_any_role(*args)
       users = []
-      args.each do |arg|
-        if arg.is_a? Hash
-          users_to_add = self.with_role(arg[:name], arg[:resource])
-        elsif arg.is_a?(String) || arg.is_a?(Symbol)
-          users_to_add = self.with_role(arg)
-        else
-          raise ArgumentError, "Invalid argument type: only hash or string or symbol allowed"
-        end
+      parse_args(args, users) do |users_to_add|
         users += users_to_add
       end
       users.uniq
+    end
+  end
+  
+  private
+  
+  def parse_args(args, users, &block)
+    args.each do |arg|
+      if arg.is_a? Hash
+        users_to_add = self.with_role(arg[:name], arg[:resource])
+      elsif arg.is_a?(String) || arg.is_a?(Symbol)
+        users_to_add = self.with_role(arg)
+      else
+        raise ArgumentError, "Invalid argument type: only hash or string or symbol allowed"
+      end
+      block.call(users_to_add)
     end
   end
 end
