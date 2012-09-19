b898a2bfd647cbb64ac62ad9e10eac46a57cd4c7
Delwyn de Villiers <delwyn.d@gmail.com>
2012-09-19 14:09:06 +0100

    White spaces

--- spec/rolify/shared_examples/shared_examples_for_remove_role.rb
+++ spec/rolify/shared_examples/shared_examples_for_remove_role.rb
@@ -3,7 +3,7 @@ shared_examples_for "#remove_role_examples" do |param_name, param_method|
     context "removing a global role", :scope => :global do
       context "being a global role of the user" do
         it { expect { subject.remove_role("admin".send(param_method)) }.to change { subject.roles.size }.by(-1) }
-        
+
         it { subject.has_role?("admin".send(param_method)).should be_false }
         it { subject.has_role?("staff".send(param_method)).should be_true }
         it { subject.has_role?("manager".send(param_method), Group).should be_true }
@@ -11,10 +11,10 @@ shared_examples_for "#remove_role_examples" do |param_name, param_method|
         it { subject.has_role?("moderator".send(param_method), Group.last).should be_true }
         it { subject.has_role?("anonymous".send(param_method), Forum.first).should be_true }
       end
-      
+
       context "being a class scoped role to the user" do
         it { expect { subject.remove_role("manager".send(param_method)) }.to change { subject.roles.size }.by(-1) }
-        
+
         it { subject.has_role?("admin".send(param_method)).should be_false }
         it { subject.has_role?("staff".send(param_method)).should be_true }
         it { subject.has_role?("manager".send(param_method), Group).should be_false }
@@ -22,10 +22,10 @@ shared_examples_for "#remove_role_examples" do |param_name, param_method|
         it { subject.has_role?("moderator".send(param_method), Group.last).should be_true }
         it { subject.has_role?("anonymous".send(param_method), Forum.first).should be_true }
       end
-      
+
       context "being instance scoped roles to the user" do
         it { expect { subject.remove_role("moderator".send(param_method)) }.to change { subject.roles.size }.by(-2) }
-        
+
         it { subject.has_role?("admin".send(param_method)).should be_false }
         it { subject.has_role?("staff".send(param_method)).should be_true }
         it { subject.has_role?("manager".send(param_method), Group).should be_false }
@@ -33,19 +33,19 @@ shared_examples_for "#remove_role_examples" do |param_name, param_method|
         it { subject.has_role?("moderator".send(param_method), Group.last).should be_false }
         it { subject.has_role?("anonymous".send(param_method), Forum.first).should be_true }
       end
-      
+
       context "not being a role of the user" do
         it { expect { subject.remove_role("superhero".send(param_method)) }.not_to change { subject.roles.size } }
       end
-      
+
       context "used by another user" do
         before do
           user = user_class.last
           user.add_role "staff".send(param_method)
         end
-        
+
         it { expect { subject.remove_role("staff".send(param_method)) }.not_to change { role_class.count } }
-        
+
         it { subject.has_role?("admin".send(param_method)).should be_false }
         it { subject.has_role?("staff".send(param_method)).should be_false }
         it { subject.has_role?("manager".send(param_method), Group).should be_false }
@@ -67,10 +67,10 @@ shared_examples_for "#remove_role_examples" do |param_name, param_method|
       context "being a global role of the user" do
         it { expect { subject.remove_role("warrior".send(param_method), Forum) }.not_to change{ subject.roles.size } }
       end
-      
+
       context "being a class scoped role to the user" do
         it { expect { subject.remove_role("manager".send(param_method), Forum) }.to change{ subject.roles.size }.by(-1) }
-        
+
         it { subject.has_role?("warrior").should be_true }
         it { subject.has_role?("manager", Forum).should be_false }
         it { subject.has_role?("player", Forum).should be_true }
@@ -78,10 +78,10 @@ shared_examples_for "#remove_role_examples" do |param_name, param_method|
         it { subject.has_role?("moderator".send(param_method), Group.last).should be_true }
         it { subject.has_role?("anonymous".send(param_method), Forum.first).should be_true }
       end
-      
+
       context "being instance scoped role to the user" do
         it { expect { subject.remove_role("moderator".send(param_method), Forum) }.to change { subject.roles.size }.by(-1) }
-        
+
         it { subject.has_role?("warrior").should be_true }
         it { subject.has_role?("manager", Forum).should be_false }
         it { subject.has_role?("player", Forum).should be_true }
@@ -89,7 +89,7 @@ shared_examples_for "#remove_role_examples" do |param_name, param_method|
         it { subject.has_role?("moderator".send(param_method), Group.last).should be_true }
         it { subject.has_role?("anonymous".send(param_method), Forum.first).should be_true }
       end
-      
+
       context "not being a role of the user" do
         it { expect { subject.remove_role("manager".send(param_method), Group) }.not_to change { subject.roles.size } }
       end
@@ -99,20 +99,20 @@ shared_examples_for "#remove_role_examples" do |param_name, param_method|
       context "being a global role of the user" do
         it { expect { subject.remove_role("soldier".send(param_method), Group.first) }.not_to change { subject.roles.size } }
       end
-      
+
       context "being a class scoped role to the user" do
         it { expect { subject.remove_role("visitor".send(param_method), Forum.first) }.not_to change { subject.roles.size } }
       end
-      
+
       context "being instance scoped role to the user" do
         it { expect { subject.remove_role("moderator".send(param_method), Forum.first) }.to change { subject.roles.size }.by(-1) }
-        
+
         it { subject.has_role?("soldier").should be_true }
         it { subject.has_role?("visitor", Forum).should be_true }
         it { subject.has_role?("moderator", Forum.first).should be_false }
         it { subject.has_role?("anonymous", Forum.last).should be_true }
       end
-      
+
       context "not being a role of the user" do
         it { expect { subject.remove_role("anonymous".send(param_method), Forum.first) }.not_to change { subject.roles.size } }
       end
