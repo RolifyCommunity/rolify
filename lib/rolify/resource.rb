72bd315245612e3f6d45e2a8a3a46921e00b6d7b
Florent Monbillard <f.monbillard@gmail.com>
2013-02-08 17:48:11 -0500

    fixed compatibility issue with Squeel gem when using symbols as role name closes #79 closes #124

--- lib/rolify/resource.rb
+++ lib/rolify/resource.rb
@@ -8,11 +8,16 @@ module Rolify
       def find_roles(role_name = nil, user = nil)
         roles = user && (user != :any) ? user.roles : self.role_class
         roles = roles.where(:resource_type => self.to_s)
-        roles = roles.where(:name => role_name) if role_name && (role_name != :any)
+        roles = roles.where(:name => role_name.to_s) if role_name && (role_name != :any)
         roles
       end
 
       def with_role(role_name, user = nil)
+        if role_name.is_a? Array
+          role_name.map!(&:to_s)
+        else
+          role_name = role_name.to_s
+        end
         resources = self.adapter.resources_find(self.role_table_name, self, role_name)
         user ? self.adapter.in(resources, user, role_name) : resources
       end
