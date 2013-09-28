3b022dda96b606b339c705025cf7487d78c30522
Karen Lundgren <karen.e.lundgren@gmail.com>
2013-09-27 18:00:29 -0600

    Fixed Rails4 deprecation warning.

--- lib/rolify/adapters/active_record/role_adapter.rb
+++ lib/rolify/adapters/active_record/role_adapter.rb
@@ -9,7 +9,7 @@ module Rolify
       end
 
       def find_or_create_by(role_name, resource_type = nil, resource_id = nil)
-        role_class.where(:name => role_name, :resource_type => resource_type, :resource_id => resource_id).first_or_create
+        role_class.find_or_create_by(:name => role_name, :resource_type => resource_type, :resource_id => resource_id)
       end
 
       def add(relation, role)
