17317c0478013fe9cd5134ccc04693573a9757af
enricostano <ocirne.onats@gmail.com>
2014-02-08 21:46:59 +0100

    Removed HABTM join table and added has_many through association

--- lib/generators/rolify/templates/role-active_record.rb
+++ lib/generators/rolify/templates/role-active_record.rb
@@ -4,8 +4,8 @@ class <%= role_cname.camelize %> < ActiveRecord::Base
     <%= table_prefix(role_cname) %>_
   end
 <% end %>
-  has_and_belongs_to_many :<%= user_cname.tableize %>, :join_table => :<%= "#{table_name(user_cname, true)}_#{table_name(role_cname, true)}" %>
+  belongs_to <%= user_cname.singularize %>
   belongs_to :resource, :polymorphic => true
-  
+
   scopify
 end
