e6c463ee56f2430d6d70b0bce13cbd605d2c3c51
Joel AZEMAR <joel.azemar@gmail.com>
2014-09-02 11:56:52 +0200

    avoid warning

--- spec/rolify/shared_examples/shared_examples_for_roles.rb
+++ spec/rolify/shared_examples/shared_examples_for_roles.rb
@@ -13,7 +13,7 @@ shared_examples_for Rolify::Role do
     Rolify.dynamic_shortcuts = false
     rolify_options = { :role_cname => role_class.to_s }
     rolify_options[:role_join_table_name] = join_table if defined? join_table
-    user_class.rolify rolify_options
+    silence_warnings { user_class.rolify rolify_options }
     role_class.destroy_all
     Forum.resourcify :roles, :role_cname => role_class.to_s
     Group.resourcify :roles, :role_cname => role_class.to_s
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,3 +33,11 @@ def provision_user(user, roles)
   end
   user
 end
+
+def silence_warnings(&block)
+  warn_level = $VERBOSE
+  $VERBOSE = nil
+  result = block.call
+  $VERBOSE = warn_level
+  result
+end
