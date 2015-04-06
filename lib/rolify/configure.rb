04d0dfd01c943cd64f5f2028c51ed574efe3f270
Ben Atkins <batkinz@gmail.com>
2015-04-06 17:39:50 -0400

    Allow all rake tasks with the 'assets' prefix to execute without the db config sanity check

--- lib/rolify/configure.rb
+++ lib/rolify/configure.rb
@@ -52,7 +52,7 @@ module Rolify
     private
 
     def sanity_check(role_cnames)
-      return true if "assets:precompile"==ARGV[0]
+      return true if (ARGV[0] =~ /assets:/) == 0
 
       role_cnames = [ "Role" ] if role_cnames.empty?
       role_cnames.each do |role_cname|
