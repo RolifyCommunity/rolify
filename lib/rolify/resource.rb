4185beb512651b018296573a20f4a7222de84605
Brian Graham <brian.graham@exigis.com>
2015-06-08 10:49:56 -0400

    Rename with_role for resource.

--- lib/rolify/resource.rb
+++ lib/rolify/resource.rb
@@ -9,7 +9,7 @@ module Rolify
         self.adapter.find_roles(role_name, self, user)
       end
 
-      def with_role(role_name, user = nil)
+      def find_as(role_name, user = nil)
         if role_name.is_a? Array
           role_name.map!(&:to_s)
         else
@@ -19,13 +19,13 @@ module Rolify
         resources = self.adapter.resources_find(self.role_table_name, self, role_name) #.map(&:id)
         user ? self.adapter.in(resources, user, role_name) : resources
       end
-      alias :with_roles :with_role
+      alias :find_as_multiple :find_as
 
 
-      def without_role(role_name, user = nil)
-        self.adapter.all_except(self, self.with_role(role_name, user))
+      def except_as(role_name, user = nil)
+        self.adapter.all_except(self, self.find_as(role_name, user))
       end
-      alias :without_roles :without_role
+      alias :except_as_multiple :except_as
 
 
 
