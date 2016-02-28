a417a42e06eff8f860eee9dec48371b70a5a43b9
Lorefnon <lorefnon@gmail.com>
2016-02-29 02:18:15 +0530

    Make resource association optional by default

--- lib/generators/active_record/rolify_generator.rb
+++ lib/generators/active_record/rolify_generator.rb
@@ -56,7 +56,10 @@ module ActiveRecord
       def model_content
         content = <<RUBY
   has_and_belongs_to_many :%{user_cname}, :join_table => :%{join_table}
-  belongs_to :resource, :polymorphic => true
+
+  belongs_to :resource,
+             :polymorphic => true,
+             :optional => true
 
   validates :resource_type,
             :inclusion => { :in => Rolify.resource_types },
