46941421ce2732c7c97d8811a6b34fbad4646743
Sergey Alekseev <sergey@asoft.co>
2018-12-29 20:41:44 +0530

    significantly optimize #without_role
    Imagine a big production application with millions of users.
    
    The previous version generates 3 queries for `User.without_role(:admin)`:
    1) For `user.all` in [`#all_except`](https://github.com/RolifyCommunity/rolify/blob/522c0d7d84a70dd3576b569edf19f9f6cb118a61/lib/rolify/adapters/active_record/role_adapter.rb#L83):
    ```sql
    SELECT "users".* FROM "users"
    ```
    2) For `self.with_role(role_name, resource)` in [`#without_role`](https://github.com/RolifyCommunity/rolify/blob/522c0d7d84a70dd3576b569edf19f9f6cb118a61/lib/rolify/finders.rb#L8):
    ```sql
    SELECT "users".* FROM "users" INNER JOIN "users_roles" ON "users_roles"."user_id" = "users"."id" INNER JOIN "roles" ON "roles"."id" = "users_roles"."role_id" WHERE (((roles.name = 'admin') AND (roles.resource_type IS NULL) AND (roles.resource_id IS NULL)))
    ```
    3) For `user.where(prime_key => (user.all - excluded_obj).map(&prime_key))` in [`#all_except`](https://github.com/RolifyCommunity/rolify/blob/522c0d7d84a70dd3576b569edf19f9f6cb118a61/lib/rolify/adapters/active_record/role_adapter.rb#L83):
    ```sql
    SELECT "users".* FROM "users" WHERE "users"."id" IN (1, 2, ..., N)
    ```
    where `N` is a number of users without the `admin` role.
    
    While a number of values in the `IN` clause for the third query can be huge there is even a worse problem. First, `user.all - excluded_obj` creates an `Array` of users and then `(user.all - excluded_obj).map(&prime_key)` calls a primary key method on every object in this array.
    
    From tests
    ---
    
    Before (3 queries):
    ```sql
    SELECT "customers".* FROM "customers"
    SELECT "customers".* FROM "customers" INNER JOIN "customers_privileges" ON "customers_privileges"."customer_id" = "customers"."id" INNER JOIN "privileges" ON "privileges"."id" = "customers_privileges"."privilege_id" WHERE (((privileges.name = 'admin') AND (privileges.resource_type IS NULL) AND (privileges.resource_id IS NULL)))
    SELECT "customers".* FROM "customers" WHERE "customers"."id" IN (2, 3, 4)
    ```
    
    After (1 query with a subquery):
    ```sql
    SELECT "customers".* FROM "customers" WHERE ("customers"."id" NOT IN (SELECT "customers"."id" FROM "customers" INNER JOIN "customers_privileges" ON "customers_privileges"."customer_id" = "customers"."id" INNER JOIN "privileges" ON "privileges"."id" = "customers_privileges"."privilege_id" WHERE (((privileges.name = 'admin') AND (privileges.resource_type IS NULL) AND (privileges.resource_id IS NULL)))))
    ```

--- lib/rolify/adapters/active_record/role_adapter.rb
+++ lib/rolify/adapters/active_record/role_adapter.rb
@@ -74,8 +74,7 @@ module Rolify
       end
 
       def all_except(user, excluded_obj)
-        prime_key = user.primary_key.to_sym
-        user.where(prime_key => (user.all - excluded_obj).map(&prime_key))
+        user.where.not(user.primary_key => excluded_obj)
       end
 
       private
