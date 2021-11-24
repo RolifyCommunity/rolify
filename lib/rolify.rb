d8424e8462140d303ae24341a19b16263e349e4f
Raphael <raphael.claasen@gmail.com>
2021-11-24 20:01:04 +0100

    Add support for polymorphic associations in the join table of the `rolify` method

--- lib/rolify.rb
+++ lib/rolify.rb
@@ -26,7 +26,10 @@ module Rolify
     rolify_options = { :class_name => options[:role_cname].camelize }
     rolify_options.merge!(options.reject{ |k,v| ![ :before_add, :after_add, :before_remove, :after_remove, :inverse_of ].include? k.to_sym })
 
-    has_many self.role_join_table_name.to_sym
+    join_table_options = { }
+    join_table_options.merge!(options.reject{ |k,v| ![ :as ].include? k.to_sym })
+
+    has_many self.role_join_table_name.to_sym, **join_table_options
     has_many :roles, through: self.role_join_table_name.to_sym, **rolify_options
 
     self.adapter = Rolify::Adapter::Base.create("role_adapter", self.role_cname, self.name)
